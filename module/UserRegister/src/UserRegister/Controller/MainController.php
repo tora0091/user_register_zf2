f9254090b0ec6c243a1a0242da6c869b515a9f66
tora0091 <tora0091@yahoo.co.jp>
2016-12-02 23:27:24 +0900

    View取得まわり修正

--- module/UserRegister/src/UserRegister/Controller/MainController.php
+++ module/UserRegister/src/UserRegister/Controller/MainController.php
@@ -6,8 +6,13 @@ use Zend\View\Model\ViewModel;
 
 class MainController extends AbstractController
 {
+    /** @var tamplate index */
+    const TAMPLATE_INDEX = 'user-register/main/index';
+    
     public function indexAction()
     {
-        return new ViewModel();
+        $view = new ViewModel();
+        $view->setTemplate(self::TAMPLATE_INDEX);
+        return $view;
     }
 }
