5b34b130cff9d5fcbf6692142355854e5d955db3
tora0091 <tora0091@yahoo.co.jp>
2016-12-02 23:27:46 +0900

    コンテナ共通トレイと追加

---
+++ module/UserRegister/src/UserRegister/Utils/ContainerTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace UserRegister\Utils;
+
+use Zend\Session\Container;
+
+trait ContainerTrait
+{
+    /**
+     * セッション情報取得
+     * @param string $namespace セッション名
+     * @return Container
+     */
+    public function getContainer($namespace = null)
+    {
+        if ($namespace === null) {
+            $namespace = get_class($this);
+        }
+        return new Container($namespace);
+    }
+    
+    /**
+     * セッションクリア
+     * @param string $namespace
+     * @return void
+     */
+    public function clearContainer($namespace = null)
+    {
+        $this->getContainer($namespace)->getManager()->getStorage()->clear($namespace);
+    }
+}
