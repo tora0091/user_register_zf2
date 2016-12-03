2227f72ae6446ced84ea88aed52fb9e05ee19563
tora0091 <tora0091@yahoo.co.jp>
2016-12-03 23:23:52 +0900

    バリデーション項目追加

--- module/UserRegister/src/UserRegister/Common/Messages.php
+++ module/UserRegister/src/UserRegister/Common/Messages.php
@@ -4,5 +4,23 @@ namespace UserRegister\Common;
 
 class Messages
 {
-    const INVAL_NUMBER_FORMAT = '社員番号の形式が異なります';
+    // バリデーションメッセージ
+    
+    // 社員番号
+    const NUMBER_INVAL_FORMAT = "社員番号の形式が異なります";
+    const NUMBER_IS_EMPTY = "社員番号は必須です";
+    
+    // 名前（姓）
+    const FAMILY_NAME_IS_EMPTY = "名前（姓）は必須です";
+
+    // 名前（名）
+    const LAST_NAME_IS_EMPTY = "名前（名）は必須です";
+    
+    // 名前（セイ）
+    const FAMILY_NAME_KANA_IS_EMPTY = "名前（セイ）は必須です";
+
+    // 名前（メイ）
+    const LAST_NAME_KANA_IS_EMPTY = "名前（メイ）は必須です";
+    
+    
 }
--- module/UserRegister/src/UserRegister/Form/InputFilter/Register/RegisterInputFilter.php
+++ module/UserRegister/src/UserRegister/Form/InputFilter/Register/RegisterInputFilter.php
@@ -3,11 +3,14 @@
 namespace UserRegister\Form\InputFilter\Register;
 
 use UserRegister\Form\InputFilter\AbstractInputFilter;
+use UserRegister\Common\Messages;
+use Zend\Validator\NotEmpty;
 
 class RegisterInputFilter extends AbstractInputFilter
 {
     public function __construct()
     {
+        // 社員番号
         $this->add([
             'name' => 'number',
             'required' => true,
@@ -17,7 +20,123 @@ class RegisterInputFilter extends AbstractInputFilter
             ],
             'validators' => [
                 [
+                    'name' => 'NotEmpty',
+                    'options' => [
+                        'messages' => [
+                            NotEmpty::isEmpty => Messages::NUMBER_IS_EMPTY,
+                        ],
+                    ],
+                ],
+                [
                     'name' => 'UserRegister\Form\Validator\NumberFormat',
+                ],
+            ],
+        ]);
+
+        // 名前（姓）        
+        $this->add([
+            'name' => 'family_name',
+            'required' => true,
+            'filters' => [
+                ['name' => 'StripTags'],
+                ['name' => 'StringTrim'],
+            ],
+            'validators' => [
+                [
+                    'name' => 'NotEmpty',
+                    'options' => [
+                        'messages' => [
+                            NotEmpty::isEmpty => Messages::FAMILY_NAME_IS_EMPTY,
+                        ],
+                    ],
+                ],
+                [
+                    'name' => 'StringLength',
+                    'options' => [
+                        'min' => 1,
+                        'max' => 20,
+                    ],
+                ],
+            ],
+        ]);
+
+        // 名前（名）
+        $this->add([
+            'name' => 'last_name',
+            'required' => true,
+            'filters' => [
+                ['name' => 'StripTags'],
+                ['name' => 'StringTrim'],
+            ],
+            'validators' => [
+                [
+                    'name' => 'NotEmpty',
+                    'options' => [
+                        'messages' => [
+                            NotEmpty::isEmpty => Messages::LAST_NAME_IS_EMPTY,
+                        ],
+                    ],
+                ],
+                [
+                    'name' => 'StringLength',
+                    'options' => [
+                        'min' => 1,
+                        'max' => 20,
+                    ],
+                ],
+            ],
+        ]);
+
+        // 名前（セイ）
+        $this->add([
+            'name' => 'family_name_kana',
+            'required' => true,
+            'filters' => [
+                ['name' => 'StripTags'],
+                ['name' => 'StringTrim'],
+            ],
+            'validators' => [
+                [
+                    'name' => 'NotEmpty',
+                    'options' => [
+                        'messages' => [
+                            NotEmpty::isEmpty => Messages::FAMILY_NAME_KANA_IS_EMPTY,
+                        ],
+                    ],
+                ],
+                [
+                    'name' => 'StringLength',
+                    'options' => [
+                        'min' => 1,
+                        'max' => 40,
+                    ],
+                ],
+            ],
+        ]);
+
+        // 名前（メイ）
+        $this->add([
+            'name' => 'last_name_kana',
+            'required' => true,
+            'filters' => [
+                ['name' => 'StripTags'],
+                ['name' => 'StringTrim'],
+            ],
+            'validators' => [
+                [
+                    'name' => 'NotEmpty',
+                    'options' => [
+                        'messages' => [
+                            NotEmpty::isEmpty => Messages::LAST_NAME_KANA_IS_EMPTY,
+                        ],
+                    ],
+                ],
+                [
+                    'name' => 'StringLength',
+                    'options' => [
+                        'min' => 1,
+                        'max' => 40,
+                    ],
                 ],
             ],
         ]);
