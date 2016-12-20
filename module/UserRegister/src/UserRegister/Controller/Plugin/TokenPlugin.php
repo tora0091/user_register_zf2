802fa97dd9313542c0295276d2196574773bc03f
tora0091 <tora0091@yahoo.co.jp>
2016-12-20 23:19:45 +0900

    ワンタイムトークン用のプラグイン作成

--- module/UserRegister/src/UserRegister/Controller/Plugin/TokenPlugin.php
+++ module/UserRegister/src/UserRegister/Controller/Plugin/TokenPlugin.php
@@ -2,17 +2,45 @@
 
 namespace UserRegister\Controller\Plugin;
 
+use UserRegister\Common\Exception\NotTokenException;
 use Zend\Mvc\Controller\Plugin\AbstractPlugin;
+use Zend\Http\Request;
 use Zend\Session\Container;
 use Zend\View\Model\ViewModel;
 
 class TokenPlugin extends AbstractPlugin
 {
-    public function __invoke(ViewModel $view, Container $session)
+    public function __invoke()
+    {
+        return $this;
+    }
+    
+    /**
+     * setToken
+     * @param ViewModel $view
+     * @param Container $session
+     */
+    public function setToken(ViewModel $view, Container $session)
     {
         $token = md5(uniqid(rand(), true));
         
         $session->token = $token;
         $view->setVariable('token', $token);
+    }
+
+    /**
+     * checkToken
+     * @param Request $request
+     * @param Container $session
+     * @throws NotTokenException
+     */    
+    public function checkToken(Request $request, Container $session)
+    {
+        $postToken = $request->getPost('token');
+        $sessionToken = $session->token;
+        
+        if ($postToken !== $sessionToken) {
+            throw new NotTokenException();
+        }
     }
 }
