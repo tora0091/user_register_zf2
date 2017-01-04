554c4294eb2b559e6a46a5373e5e54efe8c739c8
tora0091 <tora0091@yahoo.co.jp>
2017-01-04 13:56:40 +0900

    バリデーションのテストコード追加

--- module/UserRegister/test/Form/InputFilter/Register/RegisterInputFilterTest.php
+++ module/UserRegister/test/Form/InputFilter/Register/RegisterInputFilterTest.php
@@ -5,6 +5,8 @@ namespace UserRegisterTest\Form\InputFilter\Register;
 use UserRegister\Common\Messages;
 use UserRegister\Form\InputFilter\Register\RegisterInputFilter;
 use Zend\Validator\NotEmpty;
+use Zend\Validator\StringLength;
+use UserRegister\Form\Validator\NumberFormat;
 
 class RegisterInputFilterTest extends \PHPUnit_Framework_TestCase
 {
@@ -58,6 +60,68 @@ class RegisterInputFilterTest extends \PHPUnit_Framework_TestCase
         $message = $input->getMessages();
         $this->assertThat($message, $this->arrayHasKey('number'));
         $this->assertEquals($message['number'], [NotEmpty::IS_EMPTY => Messages::NUMBER_IS_EMPTY]);
+        
+        // 社員番号フォーマットチェック
+        $numbers = [
+            'N1234',        // 5桁
+            'N123456',      // 7桁
+            '123456',       // 数値のみ
+            'NNNNNN',       // 英字のみ
+            'N1234A',       // 末尾英字
+            'NN1234',       // 先頭以外英字
+            'あいうえお',   // 全角文字
+            'アカサタナ',   // 全角カタカナ
+            'ｱｶｻﾀﾅ'         // 半角カタカナ
+        ];
+        foreach ($numbers as $number) {
+            // 異なるフォーマット
+            $input = $this->createInputFilter(['number' => $number]);
+            $this->assertFalse($input->isValid());
+
+            $message = $input->getMessages();
+            $this->assertThat($message, $this->arrayHasKey('number'));
+            $this->assertEquals($message['number'], [NumberFormat::INVALID => Messages::NUMBER_INVAL_FORMAT]);
+        }
+    }
+
+    /**
+     * お名前（姓）の検証
+     */    
+    public function testFamilyName()
+    {
+        // 必須チェック
+        $input = $this->createInputFilter(['family_name' => '']);
+        $this->assertFalse($input->isValid());
+
+        $message = $input->getMessages();
+        $this->assertThat($message, $this->arrayHasKey('family_name'));
+        $this->assertEquals($message['family_name'], [NotEmpty::IS_EMPTY => Messages::FAMILY_NAME_IS_EMPTY]);
+
+        // 文字列長チェック（1-20）0文字の場合は必須チェックでエラーとなる
+        $familyNameOkList = [
+            'あ',
+            'あいうえおあいうえおあいうえおあいうえお',
+            'abcdeabcdeabcdeabcde',
+            'ＡＢＣＤＥＡＢＣＤＥＡＢＣＤＥＡＢＣＤＥ',
+        ];
+        foreach ($familyNameOkList as $name) {
+            $input = $this->createInputFilter(['family_name' => $name]);
+            $this->assertTrue($input->isValid());
+        }
+        
+        $familyNameNgList = [
+            'あいうえおあいうえおあいうえおあいうえおあ',
+            'abcdeabcdeabcdeabcdea',
+            'ＡＢＣＤＥＡＢＣＤＥＡＢＣＤＥＡＢＣＤＥＡ',
+        ];
+        foreach ($familyNameNgList as $name) {
+            $input = $this->createInputFilter(['family_name' => $name]);
+            $this->assertFalse($input->isValid());
+
+            $message = $input->getMessages();
+            $this->assertThat($message, $this->arrayHasKey('family_name'));
+            $this->assertEquals($message['family_name'][StringLength::TOO_LONG][StringLength::TOO_LONG], Messages::FAMILY_NAME_TOO_LONG);
+        }
     }
 
     private function createInputFilter(array $data = [])
