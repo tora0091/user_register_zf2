ebfd6f3a16d25e6862c9faf3e0d053d224b04947
tora0091 <tora0091@yahoo.co.jp>
2017-01-05 22:29:18 +0900

    バリデーションテストコード追加

--- module/UserRegister/test/Form/InputFilter/Register/RegisterInputFilterTest.php
+++ module/UserRegister/test/Form/InputFilter/Register/RegisterInputFilterTest.php
@@ -4,6 +4,8 @@ namespace UserRegisterTest\Form\InputFilter\Register;
 
 use UserRegister\Common\Messages;
 use UserRegister\Form\InputFilter\Register\RegisterInputFilter;
+use UserRegister\Form\Validator\StringKatakana;
+use Zend\Validator\Digits;
 use Zend\Validator\NotEmpty;
 use Zend\Validator\StringLength;
 use UserRegister\Form\Validator\NumberFormat;
@@ -122,6 +124,273 @@ class RegisterInputFilterTest extends \PHPUnit_Framework_TestCase
             $this->assertThat($message, $this->arrayHasKey('family_name'));
             $this->assertEquals($message['family_name'][StringLength::TOO_LONG][StringLength::TOO_LONG], Messages::FAMILY_NAME_TOO_LONG);
         }
+    }
+
+    /**
+     * お名前（名）の検証
+     */    
+    public function testLastName()
+    {
+        // 必須チェック
+        $input = $this->createInputFilter(['last_name' => '']);
+        $this->assertFalse($input->isValid());
+
+        $message = $input->getMessages();
+        $this->assertThat($message, $this->arrayHasKey('last_name'));
+        $this->assertEquals($message['last_name'], [NotEmpty::IS_EMPTY => Messages::LAST_NAME_IS_EMPTY]);
+
+        // 文字列長チェック（1-20）0文字の場合は必須チェックでエラーとなる
+        $lastNameOkList = [
+            'あ',
+            'あいうえおあいうえおあいうえおあいうえお',
+            'abcdeabcdeabcdeabcde',
+            'ＡＢＣＤＥＡＢＣＤＥＡＢＣＤＥＡＢＣＤＥ',
+        ];
+        foreach ($lastNameOkList as $name) {
+            $input = $this->createInputFilter(['last_name' => $name]);
+            $this->assertTrue($input->isValid());
+        }
+        
+        $lastNameNgList = [
+            'あいうえおあいうえおあいうえおあいうえおあ',
+            'abcdeabcdeabcdeabcdea',
+            'ＡＢＣＤＥＡＢＣＤＥＡＢＣＤＥＡＢＣＤＥＡ',
+        ];
+        foreach ($lastNameNgList as $name) {
+            $input = $this->createInputFilter(['last_name' => $name]);
+            $this->assertFalse($input->isValid());
+
+            $message = $input->getMessages();
+            $this->assertThat($message, $this->arrayHasKey('last_name'));
+            $this->assertEquals($message['last_name'][StringLength::TOO_LONG][StringLength::TOO_LONG], Messages::LAST_NAME_TOO_LONG);
+        }
+    }
+
+    /**
+     * お名前（セイ）の検証
+     */    
+    public function testFamilyNameKana()
+    {
+        // 必須チェック
+        $input = $this->createInputFilter(['family_name_kana' => '']);
+        $this->assertFalse($input->isValid());
+
+        $message = $input->getMessages();
+        $this->assertThat($message, $this->arrayHasKey('family_name_kana'));
+        $this->assertEquals($message['family_name_kana'], [NotEmpty::IS_EMPTY => Messages::FAMILY_NAME_KANA_IS_EMPTY]);
+
+        // 文字列長チェック（1-40）0文字の場合は必須チェックでエラーとなる
+        $okList = [
+            'ア',
+            'アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオ',
+        ];
+        foreach ($okList as $name) {
+            $input = $this->createInputFilter(['family_name_kana' => $name]);
+            $this->assertTrue($input->isValid());
+        }
+        
+        $ngList = [
+            'アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオア',
+        ];
+        foreach ($ngList as $name) {
+            $input = $this->createInputFilter(['family_name_kana' => $name]);
+            $this->assertFalse($input->isValid());
+
+            $message = $input->getMessages();
+            $this->assertThat($message, $this->arrayHasKey('family_name_kana'));
+            $this->assertEquals($message['family_name_kana'][StringLength::TOO_LONG][StringLength::TOO_LONG], Messages::FAMILY_NAME_KANA_TOO_LONG);
+        }
+        
+        // カタカナ文字以外はエラーとする
+        $okList = [
+            'ｱｲｳｴｵ',    // 半角カタカナ、半角カタカナは全角カタカナに返還される
+        ];
+        foreach ($okList as $name) {
+            $input = $this->createInputFilter(['family_name_kana' => $name]);
+            $this->assertTrue($input->isValid());
+        }
+
+        $ngList = [
+            '12345',        // 数値
+            'abcde',        // 英字
+            'あいうえお',   // 全角文字
+            '+:*@---',      // 記号
+        ];
+        foreach ($ngList as $name) {
+            $input = $this->createInputFilter(['family_name_kana' => $name]);
+            $this->assertFalse($input->isValid());
+
+            $message = $input->getMessages();
+            $this->assertThat($message, $this->arrayHasKey('family_name_kana'));
+            $this->assertEquals($message['family_name_kana'], [StringKatakana::INVALID_ZENKAKU => Messages::INVALID_STRING_KATAKANA_ZENKAKU]);
+        }
+    }
+
+    /**
+     * お名前（セイ）の検証
+     */    
+    public function testLastNameKana()
+    {
+        // 必須チェック
+        $input = $this->createInputFilter(['last_name_kana' => '']);
+        $this->assertFalse($input->isValid());
+
+        $message = $input->getMessages();
+        $this->assertThat($message, $this->arrayHasKey('last_name_kana'));
+        $this->assertEquals($message['last_name_kana'], [NotEmpty::IS_EMPTY => Messages::LAST_NAME_KANA_IS_EMPTY]);
+
+        // 文字列長チェック（1-40）0文字の場合は必須チェックでエラーとなる
+        $okList = [
+            'ア',
+            'アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオ',
+        ];
+        foreach ($okList as $name) {
+            $input = $this->createInputFilter(['last_name_kana' => $name]);
+            $this->assertTrue($input->isValid());
+        }
+        
+        $ngList = [
+            'アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオア',
+        ];
+        foreach ($ngList as $name) {
+            $input = $this->createInputFilter(['last_name_kana' => $name]);
+            $this->assertFalse($input->isValid());
+
+            $message = $input->getMessages();
+            $this->assertThat($message, $this->arrayHasKey('last_name_kana'));
+            $this->assertEquals($message['last_name_kana'][StringLength::TOO_LONG][StringLength::TOO_LONG], Messages::LAST_NAME_KANA_TOO_LONG);
+        }
+        
+        // カタカナ文字以外はエラーとする
+        $okList = [
+            'ｱｲｳｴｵ',    // 半角カタカナ、半角カタカナは全角カタカナに返還される
+        ];
+        foreach ($okList as $name) {
+            $input = $this->createInputFilter(['last_name_kana' => $name]);
+            $this->assertTrue($input->isValid());
+        }
+
+        $ngList = [
+            '12345',        // 数値
+            'abcde',        // 英字
+            'あいうえお',   // 全角文字
+            '+:*@---',      // 記号
+        ];
+        foreach ($ngList as $name) {
+            $input = $this->createInputFilter(['last_name_kana' => $name]);
+            $this->assertFalse($input->isValid());
+
+            $message = $input->getMessages();
+            $this->assertThat($message, $this->arrayHasKey('last_name_kana'));
+            $this->assertEquals($message['last_name_kana'], [StringKatakana::INVALID_ZENKAKU => Messages::INVALID_STRING_KATAKANA_ZENKAKU]);
+        }
+    }
+    
+    public function testPhoneNumber()
+    {
+        // 桁数チェック 9桁
+        $input = $this->createInputFilter(['phone_number' => '031111222']);
+        $this->assertFalse($input->isValid());
+
+        $message = $input->getMessages();
+        $this->assertThat($message, $this->arrayHasKey('phone_number'));
+        $this->assertEquals($message['phone_number'][StringLength::TOO_SHORT][StringLength::TOO_SHORT], Messages::PHONE_NUMBER_LENGTH);
+
+        // 桁数チェック 11桁
+        $input = $this->createInputFilter(['phone_number' => '03111122229']);
+        $this->assertFalse($input->isValid());
+
+        $message = $input->getMessages();
+        $this->assertThat($message, $this->arrayHasKey('phone_number'));
+        $this->assertEquals($message['phone_number'][StringLength::TOO_LONG][StringLength::TOO_LONG], Messages::PHONE_NUMBER_LENGTH);
+
+        // 数値以外
+        $ngList = [
+            'aaaaaaaaaa',
+            'ａａａａａａａａａａ',
+            'ああああああああああ',
+            'アアアアアアアアアア',
+            'ｱｱｱｱｱｱｱｱｱｱ',
+            '０３１１１１２２２２',
+        ];
+        foreach ($ngList as $number) {
+            $input = $this->createInputFilter(['phone_number' => $number]);
+            $this->assertFalse($input->isValid());
+
+            $message = $input->getMessages();
+            $this->assertThat($message, $this->arrayHasKey('phone_number'));
+            $this->assertEquals($message['phone_number'], [Digits::NOT_DIGITS => Messages::PHONE_NUMBER_NOT_DIGITS]);
+        }
+        
+        // 空値は正常とする
+        $input = $this->createInputFilter(['phone_number' => '', 'mobile_phone_number' => '08011112222']);
+        $this->assertTrue($input->isValid());
+    }
+
+    public function testMobliePhoneNumber()
+    {
+        // 桁数チェック 10桁
+        $input = $this->createInputFilter(['mobile_phone_number' => '0801111222']);
+        $this->assertFalse($input->isValid());
+
+        $message = $input->getMessages();
+        $this->assertThat($message, $this->arrayHasKey('mobile_phone_number'));
+        $this->assertEquals($message['mobile_phone_number'][StringLength::TOO_SHORT][StringLength::TOO_SHORT], Messages::MOBILE_PHONE_NUMBER_LENGTH);
+
+        // 桁数チェック 12桁
+        $input = $this->createInputFilter(['mobile_phone_number' => '080111122229']);
+        $this->assertFalse($input->isValid());
+
+        $message = $input->getMessages();
+        $this->assertThat($message, $this->arrayHasKey('mobile_phone_number'));
+        $this->assertEquals($message['mobile_phone_number'][StringLength::TOO_LONG][StringLength::TOO_LONG], Messages::MOBILE_PHONE_NUMBER_LENGTH);
+
+        // 数値以外
+        $ngList = [
+            'aaaaaaaaaaa',
+            'ａａａａａａａａａａａ',
+            'あああああああああああ',
+            'アアアアアアアアアアア',
+            'ｱｱｱｱｱｱｱｱｱｱｱ',
+            '０８０１１１１２２２２',
+        ];
+        foreach ($ngList as $number) {
+            $input = $this->createInputFilter(['mobile_phone_number' => $number]);
+            $this->assertFalse($input->isValid());
+
+            $message = $input->getMessages();
+            $this->assertThat($message, $this->arrayHasKey('mobile_phone_number'));
+            $this->assertEquals($message['mobile_phone_number'], [Digits::NOT_DIGITS => Messages::MOBILE_PHONE_NUMBER_NOT_DIGITS]);
+        }
+        
+        // 空値は正常とする
+        $input = $this->createInputFilter(['phone_number' => '0311112222', 'mobile_phone_number' => '']);
+        $this->assertTrue($input->isValid());
+    }
+    
+    public function testPhoneNumberOrMobilePhoneNumber()
+    {
+        // 両方設定されている場合
+        $input = $this->createInputFilter(['phone_number' => '0311112222', 'mobile_phone_number' => '08011112222']);
+        $this->assertTrue($input->isValid());
+
+        // 電話番号のみの場合
+        $input = $this->createInputFilter(['phone_number' => '0311112222', 'mobile_phone_number' => '']);
+        $this->assertTrue($input->isValid());
+
+        // 携帯電話番号のみの場合
+        $input = $this->createInputFilter(['phone_number' => '', 'mobile_phone_number' => '08011112222']);
+        $this->assertTrue($input->isValid());
+
+        // 両方とも空の場合
+        $input = $this->createInputFilter(['phone_number' => '', 'mobile_phone_number' => '']);
+        $this->assertFalse($input->isValid());
+
+        $message = $input->getMessages();
+        $this->assertThat($message, $this->arrayHasKey('phone_number'));
+        $this->assertEquals($message['phone_number'], [NotEmpty::IS_EMPTY => Messages::PHONE_NUMBER_IS_EMPTY]);
+        $this->assertThat($message, $this->arrayHasKey('mobile_phone_number'));
+        $this->assertEquals($message['mobile_phone_number'], [NotEmpty::IS_EMPTY => Messages::MOBILE_PHONE_NUMBER_IS_EMPTY]);
     }
 
     private function createInputFilter(array $data = [])
