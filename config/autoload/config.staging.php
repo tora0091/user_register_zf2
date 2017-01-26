d867e9925607589065995a33f4fe41ba92340a1f
tora0091 <tora0091@yahoo.co.jp>
2017-01-26 22:23:10 +0900

    DB設定まわり修正

--- config/autoload/config.staging.php
+++ config/autoload/config.staging.php
@@ -14,6 +14,7 @@ return [
         'password' => '',
         'driver_options' => [
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
+            PDO::ATTR_PERSISTENT => true,
         ],
     ],
     'log' => [
